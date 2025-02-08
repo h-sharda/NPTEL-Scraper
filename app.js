5b6681d8c9a1f655fb1a1a7786a39c593d123c50
Vector <harshitsharda0@gmail.com>
2025-02-09 05:06:55 +0530

    add path

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const { processUser } = require('./fetch.js');  // Import the function from dataFetcher.js
 
 const app = express();
